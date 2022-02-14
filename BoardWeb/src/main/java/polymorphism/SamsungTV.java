1dc26d32bf3558ef84db7269c64b2226d004f578
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-14 21:44:57 +0900

    Feat : 생성자 인젝션 이용하기

--- BoardWeb/src/main/java/polymorphism/SamsungTV.java
+++ BoardWeb/src/main/java/polymorphism/SamsungTV.java
@@ -2,8 +2,24 @@ package polymorphism;
 
 public class SamsungTV implements TV {
 	
+	private SonySpeaker speaker;
+	
+	public void ininMethod(){
+		System.out.println("객체 생성 후 멤버변수 초기화 작업....");
+	}
+	
+	public void destoryMethod(){
+		System.out.println("객체 삭제 전에 처리할 로직 처리....");
+	}
+	
 	public SamsungTV(){
-		System.out.println("삼성tv 객체 생성.");
+		System.out.println("삼성tv 객체 생성!!");
+	}
+	
+	//생성자 인젝션 이용
+	public SamsungTV(SonySpeaker sonyspeaker){
+		System.out.println("SamsungTV(2)객체생성");
+		this.speaker = sonyspeaker;
 	}
 	
 	public void powerOn(){
@@ -14,11 +30,16 @@ public class SamsungTV implements TV {
 		System.out.println("삼성 tv 전원 끈다.");
 	}
 	
+	//삼성이 소니 스피커를 사용할때
 	public void volumUp(){
-		System.out.println("삼성 tv 소리 올린다.");
+		speaker.volumUp();
+		//System.out.println("삼성 tv 소리 올린다.");
 	}
 	
+	//삼성이 소니 스피커를 사용할때
 	public void volumDown(){
-		System.out.println("삼성 tv 소리 내린다.");
+		speaker.volumDown();
+		
+		//System.out.println("삼성 tv 소리 내린다.");
 	}
 }
---
+++ BoardWeb/src/main/java/polymorphism/SonySpeaker.java
@@ -0,0 +1,17 @@
+package polymorphism;
+
+public class SonySpeaker {
+
+	public SonySpeaker(){
+		System.out.println("소니 스피커 객체 생성!");
+	}
+	
+	public void volumUp(){
+		System.out.println("소니 스피커 소리를 올린다.");
+	}
+	
+	public void volumDown(){
+		System.out.println("소니 스피커 소리를 내린다.");
+	}
+	
+}
