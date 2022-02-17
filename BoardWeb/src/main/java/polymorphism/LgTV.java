8bbfa06bba237a548206ad1443c19484bb532c17
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-17 18:51:46 +0900

    Chore : 의존성 주입 어노테이션 @Qualifier

--- BoardWeb/src/main/java/polymorphism/LgTV.java
+++ BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -1,6 +1,7 @@
 package polymorphism;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
@@ -9,6 +10,8 @@ public class LgTV implements TV{
 	//1. 주로 변수위에 설정하여 해당 타입의 객체를 찾아서 자동으로 할당한다.
 	//2. 의존성 주입에 사용했던 Setter매소드나 생성자가 필요가 없다.
 	@Autowired
+	//3. 의존성 주입 대상이 되는 speaker 객체가 두개 이상일때 발생할 수 있는 문제를 객체를 지정함으로써 해결한다.
+	@Qualifier("sony")
 	private Speaker speaker;
 	
 	public LgTV(){
