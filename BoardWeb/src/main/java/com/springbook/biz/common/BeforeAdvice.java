81df26e601d42d8df8622d7da3faf0362ce2ff8c
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-22 17:40:30 +0900

    Chore : JoinPoint와 바인드 변수(Before어드바이스)

--- BoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,9 +1,16 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class BeforeAdvice {
 	
-	public void beforeLog(){
-		System.out.println("[사전처리] 비즈니스 로직이 수행 전에 동작!!");
+	public void beforeLog(JoinPoint jp){
+		
+		String method = jp.getSignature().getName();
+		Object[] args = jp.getArgs();
+		
+		System.out.println("[사전처리]" + method +
+				"() 메소드 ARGS 정보 : " +args[0].toString());
 	}
 	
 }
