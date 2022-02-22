85997e50bd5293f4e4eb6d440ab8ed0a36e821bb
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-22 17:46:15 +0900

    Chore : JoinPoint와 바인드 변수(After Returning어드바이스)

--- BoardWeb/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterReturningAdvice.java
@@ -1,9 +1,22 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
+import com.springbook.biz.user.UserVO;
+
 public class AfterReturningAdvice {
 	
-	public void AfterLog(){
-		System.out.println("[사후 처리] 비즈니스 로직 수행 후 동작!");
+	public void AfterLog(JoinPoint jp, Object returnObj){
+		String method = jp.getSignature().getName();
+		if(returnObj instanceof UserVO){
+			UserVO user =  (UserVO)returnObj;
+			if(user.getRole().equals("Admin")){
+				System.out.println(user.getName()+ "로그인(Admin)");
+			}
+		}
+		
+		System.out.println("[사후 처리]" + method +
+				"()메소드 리턴값 : " + returnObj.toString());
 	}
 
 }
