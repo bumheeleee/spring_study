fe86ce74df528eb123da6029d1e7ba1ca95e1076
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-26 13:59:23 +0900

    Chore : JoinPoint와 바인드 변수(After Throwing어드바이스)

--- BoardWeb/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -1,9 +1,27 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+
 public class AfterThrowingAdvice {
 
-	public void execeptionLog(){
-		System.out.println("[예외처리] 비즈니스 로직 수행 중 예외 발생!!");
+	public void exceptionLog(JoinPoint jp, Exception exceptObj){
+		
+		String method = jp.getSignature().getName();
+		
+		System.out.println("[예외처리]" + method +
+							"() 메소드 수행 중 발생된 예외 메시지  : ");
+		
+		if(exceptObj instanceof IllegalArgumentException){
+			System.out.println("부적합한 값이 입력되었습니다.");
+			
+		}else if(exceptObj instanceof NumberFormatException){
+			System.out.println("숫자 형식의 값이 아닙니다.");
+			
+		}else if(exceptObj instanceof Exception){
+			System.out.println("문제가 발생했습니다.");
+		}
+		
+		
 	}
 	
 }
--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,8 +1,10 @@
 package com.springbook.biz.common;
 
+import org.aopalliance.intercept.Joinpoint;
+
 public class LogAdvice {
 	
-	public void printLog(){
+	public void printLog(Joinpoint jp){
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작!!!");
 	}
 	
