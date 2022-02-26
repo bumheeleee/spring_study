37f132044bf49ccc12bba6be13393f14bdcb4b1b
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-26 15:05:19 +0900

    Chore : 어노테이션 기반 AOP설정(Before어드바이스)

--- BoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -1,9 +1,20 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+
+@Service
+@Aspect
 public class BeforeAdvice {
 	
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut(){}
+	
+	@Before("allPointcut()")
 	public void beforeLog(JoinPoint jp){
 		
 		String method = jp.getSignature().getName();
--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Service;
 @Aspect // LogAdvice객체를 Aspect(Pointcut + Advice)객체로 인식 
 public class LogAdvice {
 	
-	//PointCut : 비즈니스 로직 메소드중 필터링 된 메소드를 말한다.
-	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
-	public void allPointcut(){}
-	
-	
-	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
-	public void getPointcut(){}
-	
-	
-	@Before("allPointcut()")
-	//Advice : 횡단 관점 로직
-	public void printLog(){
-		System.out.println("[공통로그] 비즈니스 로직 수행 전 동작한다.");
-	}
-	
+//	//PointCut : 비즈니스 로직 메소드중 필터링 된 메소드를 말한다.
+//	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+//	public void allPointcut(){}
+//	
+//	
+//	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
+//	public void getPointcut(){}
+//	
+//	
+//	@Before("allPointcut()")
+//	//Advice : 횡단 관점 로직
+//	public void printLog(){
+//		System.out.println("[공통로그] 비즈니스 로직 수행 전 동작한다.");
+//	}
+//	
 }
