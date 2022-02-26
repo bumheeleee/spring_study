814b0e6f6ef3623b9437e457f4b54d313845d0ed
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-02-26 14:59:11 +0900

    Chore : 어노테이션 기반 AOP설정

--- BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWeb/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,11 +1,28 @@
 package com.springbook.biz.common;
 
 import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 
+@Service //LogAdvice클래스의 객체를 생성하기 위해 사용
+@Aspect // LogAdvice객체를 Aspect(Pointcut + Advice)객체로 인식 
 public class LogAdvice {
 	
-	public void printLog(Joinpoint jp){
-		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작!!!");
+	//PointCut : 비즈니스 로직 메소드중 필터링 된 메소드를 말한다.
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut(){}
+	
+	
+	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
+	public void getPointcut(){}
+	
+	
+	@Before("allPointcut()")
+	//Advice : 횡단 관점 로직
+	public void printLog(){
+		System.out.println("[공통로그] 비즈니스 로직 수행 전 동작한다.");
 	}
 	
 }
