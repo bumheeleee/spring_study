ec338e3451de1ede868cde177d4eae2ac9c2ec7a
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-03-08 02:38:45 +0900

    Chore : model-2 게시판 개발(글 목록 검색 기능 구현하기)

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -1,12 +1,16 @@
 package com.springbook.view.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
@@ -54,7 +58,7 @@ public class DispatcherServlet extends HttpServlet {
 
 			//3. 화면 네비게이션
 			if(user != null){
-				response.sendRedirect("getBoardList.jsp");
+				response.sendRedirect("getBoardList.do");
 			}else{
 				response.sendRedirect("login.jsp");
 			}
@@ -76,6 +80,18 @@ public class DispatcherServlet extends HttpServlet {
 			
 		} else if (path.equals("/getBoardList.do")) {
 			System.out.println("글 목록 검색 처리");
+			
+			//1. 사용자 입력 정보 추출(검색 기능은 나중에 구현)
+			//2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			BoardDAO boardDAO = new BoardDAO();
+			List<BoardVO> boardList = boardDAO.getBoardList(vo);
+			
+			//3.검색 결과를 세션에 저장하고 목록 화면으로 이동한다.
+			HttpSession session = request.getSession();
+			session.setAttribute("boardList", boardList);
+			response.sendRedirect("getBoardList.jsp");
+			
 			
 		}
 	}
--- BoardWeb/src/main/webapp/getBoardList.jsp
+++ BoardWeb/src/main/webapp/getBoardList.jsp
@@ -4,14 +4,8 @@
 <%@page contentType="text/html; charset=EUC-KR"%>
 
 <%
-	//1. 사용자 입력 정보 추출(검색 기능은 나중에 구현)
-	//2. DB 연동 처리
-	BoardVO vo = new BoardVO();
-	BoardDAO boardDAO = new BoardDAO();
-	List<BoardVO> boardList = boardDAO.getBoardList(vo);
-	
-	
-	//3. 응답 화면 구성
+	//세션에 저장된 글 목록을 꺼낸다.
+	List<BoardVO> boardList = (List)session.getAttribute("boardList");
 %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
