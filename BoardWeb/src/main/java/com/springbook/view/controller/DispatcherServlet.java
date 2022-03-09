bd0bd891b1658288ab137d5799e69083e28bc09e
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-03-09 23:19:08 +0900

    Chore : model-2 게시판 개발(글 수정 기능 구현)

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -89,6 +89,23 @@ public class DispatcherServlet extends HttpServlet {
 		} else if (path.equals("/updateBoard.do")) {
 			System.out.println("글 수정 처리");
 			
+			// 1. 사용자 입력 정보 추출
+			//request.setCharacterEncoding("EUC-KR");
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			String seq = request.getParameter("seq");
+
+			// 2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			vo.setTitle(title);
+			vo.setContent(content);
+			vo.setSeq(Integer.parseInt(seq));
+			BoardDAO boardDAO = new BoardDAO();
+			boardDAO.updateBoard(vo);
+
+			// 3. 화면 네비게이션
+			response.sendRedirect("getBoardList.do");
+			
 		} else if (path.equals("/deleteBoard.do")) {
 			System.out.println("글 삭제 처리");
 			
--- BoardWeb/src/main/webapp/getBoard.jsp
+++ BoardWeb/src/main/webapp/getBoard.jsp
@@ -17,7 +17,7 @@
 		<h1>±Û »ó¼¼</h1>
 		<a href="logout_proc.jsp">Log-out</a>
 		<hr>
-		<form action="updateBoard_proc.jsp" method="post">
+		<form action="updateBoard.do" method="post">
 			<input name="seq" type="hidden" value="<%=board.getSeq()%>" />
 			<table border="1" cellpadding="0" cellspacing="0">
 				<tr>
