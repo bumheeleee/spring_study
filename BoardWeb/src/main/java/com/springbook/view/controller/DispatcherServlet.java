fbe571d5881591e10b87c75bb18fc0a47d8a5393
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-03-09 23:22:52 +0900

    Chore : model-2 게시판 개발(글 삭제 기능 구현)

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -109,6 +109,18 @@ public class DispatcherServlet extends HttpServlet {
 		} else if (path.equals("/deleteBoard.do")) {
 			System.out.println("글 삭제 처리");
 			
+			// 1. 사용자 입력 정보 추출
+			String seq = request.getParameter("seq");
+
+			// 2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			vo.setSeq(Integer.parseInt(seq));
+			BoardDAO boardDAO = new BoardDAO();
+			boardDAO.deleteBoard(vo);
+
+			// 3. 화면 네비게이션
+			response.sendRedirect("getBoardList.do");
+			
 		} else if (path.equals("/getBoard.do")) {
 			System.out.println("글 상세 조회 처리");
 			
