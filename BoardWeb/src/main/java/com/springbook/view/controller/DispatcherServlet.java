2a1f55b9fcd001f4efd621e26eb9403d5455e294
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-03-09 23:15:04 +0900

    Chore : model-2 게시판 개발(글 등록 기능 구현)

--- BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -69,6 +69,23 @@ public class DispatcherServlet extends HttpServlet {
 		} else if (path.equals("/insertBoard.do")) {
 			System.out.println("글 등록 처리");
 			
+			// 1. 사용자 입력 정보 추출
+			//request.setCharacterEncoding("EUC-KR"); //인코딩과 관련된 부분은 doPost매소드에서 처리해준다.
+			String title = request.getParameter("title");
+			String writer = request.getParameter("writer");
+			String content = request.getParameter("content");
+			
+			// 2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			vo.setTitle(title);
+			vo.setWriter(writer);
+			vo.setContent(content);
+			BoardDAO boardDAO = new BoardDAO();
+			boardDAO.insertBoard(vo);
+			
+			// 3. 화면 네비게이션
+			response.sendRedirect("getBoardList.do");
+			
 		} else if (path.equals("/updateBoard.do")) {
 			System.out.println("글 수정 처리");
 			
