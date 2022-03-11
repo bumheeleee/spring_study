cc80853db4937c40e18b11d4a3560b266b2ca926
이범희 <64251951+dlqjagml@users.noreply.github.com>
2022-03-11 20:39:30 +0900

    Feat : MVC 프레임 워크 구현(글 목록 검색 구현)

--- BoardWeb/src/main/java/com/springbook/view/controller/HandlerMapping.java
+++ BoardWeb/src/main/java/com/springbook/view/controller/HandlerMapping.java
@@ -3,6 +3,7 @@ package com.springbook.view.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.springbook.view.board.GetBoardListController;
 import com.springbook.view.user.LoginController;
 
 public class HandlerMapping {
@@ -13,6 +14,7 @@ public class HandlerMapping {
 	public HandlerMapping(){
 		mappings = new HashMap<String, Controller>();
 		mappings.put("/login.do", new LoginController());
+		mappings.put("/getBoardList.do", new GetBoardListController());
 	}
 	
 	public Controller getController(String path){
